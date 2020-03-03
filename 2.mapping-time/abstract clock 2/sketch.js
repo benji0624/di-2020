20bf25a81d71114a4ccd725e989d5ce63db59bc9
benji0624 <bkim26@pratt.edu>
2020-03-03 14:52:43 -0500

    update

--- 2.mapping-time/abstract clock 2/sketch.js
+++ 2.mapping-time/abstract clock 2/sketch.js
@@ -13,16 +13,21 @@ function draw() {
   var centerX = 300;
   var centerY = 300;
   var now = clock()
+  var seasonz = map(now.season, 1,4, 0,1)
 
   background(220);
   
   ellipse(centerX, centerY, 10, 10);
-  
+
+  var angle = map(now.hour, 0,12, 0,360)
   var x = centerX + radius * cos(angle);
   var y = centerY + radius * sin(angle);
+  let from = color(218, 165, 32);
+  let to = color(72, 61, 139);
+  fill(lerpColor(from, to, seasonz))
   ellipse(x, y, 50, 50);
-
-  angle = angle + ;
+  
+  
 
 }
 
